2909c1302f8a45c3e2c481475c4ffe1d180cc434
Mycken <m0122333@gmail.com>
2021-05-12 11:06:11 +0300

    presize

--- src/main/java/com/example/pp312/Pp312Application.java
+++ src/main/java/com/example/pp312/Pp312Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Pp311Application {
+public class Pp312Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Pp311Application.class, args);
+		SpringApplication.run(Pp312Application.class, args);
 	}
 
 }
